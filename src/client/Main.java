3143a0b10707f9b75ecb2c9b6a75483a1520474e
Christoffer Øhrstrøm <chrisnave92@gmail.com>
2019-03-19 15:57:02 +0100

    intial simple configuration for intellij

---
+++ src/client/Main.java
@@ -0,0 +1,15 @@
+package client;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // TODO:
+        // read config (and override with command line args)
+        // level = server input from stdin
+        // initialState = parser(level)
+        // solution = Search(initialState)
+        // did solution work?
+        //   - then send solution to server
+        //   - otherwise throw a relevant error message
+        System.err.println("Main is not implemented yet!");
+    }
+}
