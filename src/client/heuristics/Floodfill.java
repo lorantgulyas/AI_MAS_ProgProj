afc6e760a7b15c437eac61ff869d768eee18e3fa
Peter Gönczöl <gonczolp@gmail.com>
2019-03-31 17:55:17 +0200

    Floodfill now works with large maps

--- src/client/heuristics/Floodfill.java
+++ src/client/heuristics/Floodfill.java
@@ -1,5 +1,6 @@
 package client.heuristics;
 
+import client.NotImplementedException;
 import client.Position;
 import client.State;
 import client.definitions.AHeuristic;
@@ -14,58 +15,62 @@ public class Floodfill extends AHeuristic {
 
     public Floodfill(State initialState) {
         super(initialState);
-        boolean[][] walls = initialState.getWalls();
         int rowCount = State.getRowCount();
         int colCount = State.getColCount();
 
-        int goalX = 2;
-        int goalY = 1;
-
         // matrix starts with goal coordinates, in order to make the memory layout nicer
         matrix = new int[colCount][rowCount][colCount][rowCount];
-        // for loop here
-        {
-            // preemptively fill all values with -1
-            for (int[] rowTo : matrix[goalX][goalY])
-                Arrays.fill(rowTo, UNDISCOVERED);
-
-            // if goal position is not wall
-            if (!walls[goalX][goalY]) {
-                int currentDistance = 0;
-                matrix[goalX][goalY][goalX][goalY] = currentDistance;
-                ArrayDeque<Position> toExplore = new ArrayDeque<>();
-                Position pos = new Position(goalX, goalY);
-                toExplore.add(pos);
-                while (!toExplore.isEmpty()) {
-                    pos = toExplore.pop();
-                    int x = pos.getCol(),  y = pos.getRow();
-                    currentDistance = matrix[goalX][goalY][x][y] + 1;
-
-                    // ugly shit
-                    if (!walls[x][y - 1] && matrix[goalX][goalY][x][y - 1] == UNDISCOVERED) {
-                        matrix[goalX][goalY][x][y - 1] = currentDistance;
-                        toExplore.add(new Position(x, y - 1));
-                    }
-                    if (!walls[x][y + 1] && matrix[goalX][goalY][x][y + 1] == UNDISCOVERED) {
-                        matrix[goalX][goalY][x][y + 1] = currentDistance;
-                        toExplore.add(new Position(x, y + 1));
-                    }
-                    if (!walls[x - 1][y] && matrix[goalX][goalY][x - 1][y] == UNDISCOVERED) {
-                        matrix[goalX][goalY][x - 1][y] = currentDistance;
-                        toExplore.add(new Position(x - 1, y));
-                    }
-                    if (!walls[x + 1][y] && matrix[goalX][goalY][x + 1][y] == UNDISCOVERED) {
-                        matrix[goalX][goalY][x + 1][y] = currentDistance;
-                        toExplore.add(new Position(x + 1, y));
-                    }
-                }
+        for (int goalX = 0; goalX < colCount; goalX++) {
+            for (int goalY = 0; goalY < rowCount; goalY++) {
+                fillSubMatrix(goalX, goalY, initialState);
             }
         }
 
-        printDistanceMatrix(goalX, goalY);
+        // for debug purposes
+         printMatrix(25, 25);
     }
 
-    public void printDistanceMatrix(int goalX, int goalY) {
+    private void fillSubMatrix(int goalX, int goalY, State initialState) {
+        boolean[][] walls = initialState.getWalls();
+
+        // preemptively fill all values with -1
+        for (int[] rowTo : matrix[goalX][goalY])
+            Arrays.fill(rowTo, UNDISCOVERED);
+
+        // if goal position is not wall
+        if (!walls[goalX][goalY]) {
+            int currentDistance = 0;
+            matrix[goalX][goalY][goalX][goalY] = currentDistance;
+            ArrayDeque<Position> toExplore = new ArrayDeque<>();
+            Position pos = new Position(goalX, goalY);
+            toExplore.add(pos);
+            while (!toExplore.isEmpty()) {
+                pos = toExplore.pop();
+                int x = pos.getCol(),  y = pos.getRow();
+                currentDistance = matrix[goalX][goalY][x][y] + 1;
+
+                // ugly shit
+                if (!walls[x][y - 1] && matrix[goalX][goalY][x][y - 1] == UNDISCOVERED) {
+                    matrix[goalX][goalY][x][y - 1] = currentDistance;
+                    toExplore.add(new Position(x, y - 1));
+                }
+                if (!walls[x][y + 1] && matrix[goalX][goalY][x][y + 1] == UNDISCOVERED) {
+                    matrix[goalX][goalY][x][y + 1] = currentDistance;
+                    toExplore.add(new Position(x, y + 1));
+                }
+                if (!walls[x - 1][y] && matrix[goalX][goalY][x - 1][y] == UNDISCOVERED) {
+                    matrix[goalX][goalY][x - 1][y] = currentDistance;
+                    toExplore.add(new Position(x - 1, y));
+                }
+                if (!walls[x + 1][y] && matrix[goalX][goalY][x + 1][y] == UNDISCOVERED) {
+                    matrix[goalX][goalY][x + 1][y] = currentDistance;
+                    toExplore.add(new Position(x + 1, y));
+                }
+            }
+        }
+    }
+
+    public void printMatrix(int goalX, int goalY) {
         StringBuilder s = new StringBuilder();
 
         for (int y = 0; y < State.getRowCount(); y++) {
@@ -81,13 +86,17 @@ public class Floodfill extends AHeuristic {
         System.err.println(s.toString());
     }
 
-    public int getDistance(int fromX, int fromY, int toX, int toY) {
+    public int distance(int fromX, int fromY, int toX, int toY) {
         return matrix[toX][toY][fromX][fromY];
+    }
+
+    public int distance(Position from, Position to) {
+        return matrix[to.getCol()][to.getRow()][from.getCol()][from.getRow()];
     }
 
     @Override
     public int h(AState state) {
-        return 0;
+        throw new NotImplementedException();
     }
 
 }
