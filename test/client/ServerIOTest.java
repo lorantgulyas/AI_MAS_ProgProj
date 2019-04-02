16414b4a24ad399e71b395e65cbb61464a55bc84
Peter Gönczöl <gonczolp@gmail.com>
2019-04-02 11:47:08 +0200

    Refactored ServerIO to work with tests

--- test/client/ServerIOTest.java
+++ test/client/ServerIOTest.java
@@ -2,29 +2,32 @@ package client;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerIOTest {
+    static FileReader in;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUp() throws FileNotFoundException {
+        in = new FileReader("./MAExample.lvl");
     }
 
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void readState() {
-    }
-
-    @Test
-    void sendJointAction() {
+    @AfterAll
+    static void tearDown() throws IOException {
+        in.close();
     }
 
     @Test
-    void sendComment() {
+    void readState() throws Exception {
+        BufferedReader br = new BufferedReader(in);
+        State state = ServerIO.parseState(br);
+        // assert here
+        System.err.println(state);
     }
 }
