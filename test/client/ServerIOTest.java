54c7cee70746f37cfb4743a3b4a4882a03d97cf1
Lorant Gulyas <lorant.gulyas@gmail.com>
2019-04-02 11:01:37 +0200

    init test

---
+++ test/client/ServerIOTest.java
@@ -0,0 +1,30 @@
+package client;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerIOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void readState() {
+    }
+
+    @Test
+    void sendJointAction() {
+    }
+
+    @Test
+    void sendComment() {
+    }
+}
