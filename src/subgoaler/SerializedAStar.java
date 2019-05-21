cab0e856f96358b5f6ebd635e226b3aa8a621a40
Lorant Gulyas <lorant.gulyas@gmail.com>
2019-05-21 15:19:51 +0200

    IDAStar

--- src/subgoaler/SerializedAStar.java
+++ src/subgoaler/SerializedAStar.java
@@ -30,7 +30,7 @@ public class SerializedAStar {
         Goal[] allGoals = subresult.getGoals();
 
         for (int i = 0; i < allGoals.length; i++) {
-//        for (int i = 0; i < 13; i++) {
+//        for (int i = 0; i < 1; i++) {
             Box[] allBoxes = subresult.getBoxes();
             Goal[] goalsDone = Arrays.copyOfRange(allGoals, 0, i);
             Box[] boxesDone = Arrays.copyOfRange(allBoxes, 0, i);
@@ -59,35 +59,42 @@ public class SerializedAStar {
 
             ArrayList<Command> subCmds;
 
-            try {
-                subCmds = plan(subresult);
-            } catch (BlockedException e) {
-                System.err.println("freeing path for " + allGoals[i]);
-                HashSet<Position> path = new HashSet<>();
+//            try {
+//                subCmds = plan(subresult);
+//            } catch (BlockedException e) {
+//                System.err.println("freeing path for " + allGoals[i]);
+//                HashSet<Position> path = new HashSet<>();
+//
+//                path.addAll(
+//                        ff.findPath(allGoals[i].getPosition(),
+//                        subresult.getBoxes()[i].getPosition(), subresult)
+//                );
+//                // path does not contain goal itself we have to add it manually
+//                path.add(allGoals[i].getPosition());
+//
+//                // test out agent-box stuff
+//                path.addAll(
+//                        ff.findPath(subresult.getBoxes()[i].getPosition(),
+//                                subresult.getAgents()[0].getPosition(), subresult)
+//                );
+//
+//                subCmds = freePath(subresult, new ArrayList<>(path));
+//                try {
+//                    System.err.println("trying again: " + allGoals[i]);
+//                    subCmds.addAll(plan(subresult));
+//                } catch (BlockedException ex) {
+//                    System.err.println("we failed big times");
+//                }
+//            }
 
-                path.addAll(
-                        ff.findPath(allGoals[i].getPosition(),
-                        subresult.getBoxes()[i].getPosition(), subresult)
-                );
-                // path does not contain goal itself we have to add it manually
-                path.add(allGoals[i].getPosition());
-
-                // test out agent-box stuff
-                path.addAll(
-                        ff.findPath(subresult.getBoxes()[i].getPosition(),
-                                subresult.getAgents()[0].getPosition(), subresult)
-                );
-
-                subCmds = freePath(subresult, new ArrayList<>(path));
-                try {
-                    System.err.println("trying again: " + allGoals[i]);
-                    subCmds.addAll(plan(subresult));
-                } catch (BlockedException ex) {
-                    System.err.println("we failed big times");
-                }
-            }
+            IDAStar idaStar = new IDAStar(ff);
+            subCmds = idaStar.run(subresult);
 
             cmds.addAll(subCmds);
+
+//            for (Command cmd : cmds) {
+//                System.err.println(cmd);
+//            }
         }
         return cmds;
     }
@@ -132,7 +139,7 @@ public class SerializedAStar {
         }
     }
 
-    public ArrayList<Command> plan(State init) throws BlockedException {
+    public ArrayList<Command> plan(State init) /* throws BlockedException */ {
         AStar strategy = new AStar(ff);
         strategy.addToFrontier(init);
 
@@ -141,9 +148,9 @@ public class SerializedAStar {
             explored++;
             if (iterations % 10000 == 0) {
                 System.err.println("i: " + iterations + ", " + strategy.searchStatus());
-                if (iterations == 20000) {
-                    throw new BlockedException();
-                }
+//                if (iterations == 20000) {
+//                    throw new BlockedException();
+//                }
             }
 
             if (strategy.frontierIsEmpty()) {
@@ -232,6 +239,71 @@ public class SerializedAStar {
 
         public boolean inFrontier(State n) {
             return frontierSet.contains(n);
+        }
+    }
+
+    public class IDAStar {
+        private State currentState;
+        private Floodfill ff;
+        private Boolean found = false;
+        private HashSet<State> explored = new HashSet<>();
+        private int iter = 0;
+        private final long startTime;
+
+        public IDAStar(Floodfill ff) {
+            this.ff = ff;
+            this.startTime = System.currentTimeMillis();
+        }
+
+        public ArrayList<Command> run(State root) {
+            int threshold = ff.h(root);
+            explored.add(root);
+            currentState = root;
+            while (!found) {
+                threshold = search(currentState, 0, threshold);
+            }
+
+            ArrayList<Command> res = currentState.extractPlan();
+            subresult = currentState;
+            subresult.parent = null;
+            subresult.cmd = null;
+            return res;
+
+        }
+
+        public int search(State leafNode, int g, int threshold) {
+            int f = g + ff.h(leafNode);
+            if (f > threshold) return f;
+            if (leafNode.isGoalState()) {
+                currentState = leafNode;
+                found = true;
+                return f;
+            }
+            int min = Integer.MAX_VALUE;
+            for (State n : leafNode.getExpandedStates()) {
+                if (!explored.contains(n)) {
+                    iter++;
+                    if(iter%1000000==0)
+                        System.err.println("i: " + iter + ", " + searchStatus());
+                    explored.add(n);
+                    int t = search(n, g + 1, threshold);
+                    if (found) {
+                        return f;
+                    }
+                    if (t < min) min = t;
+                    explored.remove(n);
+                }
+            }
+
+            return min;
+        }
+
+        public float timeSpent() {
+            return (System.currentTimeMillis() - this.startTime) / 1000f;
+        }
+
+        public String searchStatus() {
+            return String.format("Time: %3.2f s \t%s", this.timeSpent(), Memory.stringRep());
         }
     }
 
